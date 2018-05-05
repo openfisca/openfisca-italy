b75cfffdda05baba73c7fe5e3b8d06c365cf8103
stach <lorenzo.stacchio.dev@gmail.com>
2018-05-05 19:02:49 +0200

    Bug fix
    - Insert a return value in a variable that cause the fail of the tests.

--- openfisca_italy/variables/Imposte/IRPEF/benefici/detrazioni_fiscali_IRPEF/fonti_detrazioni_carichi_famigliari/detrazioni_coniuge_a_carico.py
+++ openfisca_italy/variables/Imposte/IRPEF/benefici/detrazioni_fiscali_IRPEF/fonti_detrazioni_carichi_famigliari/detrazioni_coniuge_a_carico.py
@@ -13,5 +13,7 @@ class detrazioni_per_conigue_a_carico(Variable):
     reference = "http://www.agenziaentrate.gov.it/wps/file/Nsilib/Nsi/Schede/Dichiarazioni/Redditi+Persone+fisiche+2018/Modello+e+istruzioni+Redditi+PF2018/Istruzioni+Redditi+Pf+-+Fascicolo+1+2018/PF1_istruzioni_2018_Ret.pdf"  # Always use the most official source
     
     def formula(person,period,parameters):
-    #reddito per detrazioni è uguale alla somma del reddito_totale_lordo_annuale - deduzione_abitazione_principale + importo_del_rendimento_nozionale_di_spettanza_dell_imprenditore
-    reddito_per_detrazioni = person('reddito_totale_lordo_annuale',period) - person('deduzione_abitazione_principale',period) + person('importo_del_rendimento_nozionale_di_spettanza_dell_imprenditore',period)
+        #reddito per detrazioni è uguale alla somma del reddito_totale_lordo_annuale - deduzione_abitazione_principale + importo_del_rendimento_nozionale_di_spettanza_dell_imprenditore
+        reddito_per_detrazioni = person('reddito_totale_lordo_annuale',period) - person('deduzione_abitazione_principale_annuale',period) + person('importo_del_rendimento_nozionale_di_spettanza_dell_imprenditore',period)
+        # detrazioni varie
+        return reddito_per_detrazioni
