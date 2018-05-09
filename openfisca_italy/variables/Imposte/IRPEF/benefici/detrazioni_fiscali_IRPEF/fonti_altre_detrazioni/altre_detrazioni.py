30cb4a783f82931d10eb9461afe4ab95c1468199
Unknown <lorenzo.stacchio.dev@gmail.com>
2018-05-09 19:29:38 +0200

    Introduce deduction for other deduction
    - Introduce python variables file `altre_detrazioni.py` that contains all the variables for the expenses and insert the formula to calculate this deduction in `sezione_spese_altre_detrazione.py`
    - Test for deduction in `altre_detrazioni_sezioneVI_quadro_RP.yaml`;

---
+++ openfisca_italy/variables/Imposte/IRPEF/benefici/detrazioni_fiscali_IRPEF/fonti_altre_detrazioni/altre_detrazioni.py
@@ -0,0 +1,62 @@
+# total gross income for one month and one year
+# Import from openfisca-core the common python objects used to code the legislation in OpenFisca
+from openfisca_core.model_api import *
+# Import the entities specifically defined for this tax and benefit system
+from openfisca_italy.entita import *
+# import numpy
+import numpy as np
+
+class altre_detrazioni_annue_totali(Variable):
+    value_type = float
+    entity = Persona
+    definition_period = YEAR
+    set_input = set_input_divide_by_period
+    label = "Detrazioni speciali per mantenimento cane guida"
+    reference = "http://www.agenziaentrate.gov.it/wps/wcm/connect/fcae4d804bb1ef709472f5d94f8d55f4/Annuario_online_Parte_III.pdf?MOD=AJPERES"  # Always use the most official source
+
+    def formula(person,period,parameters):
+        tipi_altre_detrazioni=['detrazioni_per_mantenimento_cane_guida',
+                                'altre_detrazioni',
+                                'detrazioni_per_investimenti_startup',
+                                'detrazioni_di_recupero_per_decadenza_startup',]
+        return round_(sum(person(detrazione, period) for detrazione in tipi_altre_detrazioni),2)
+
+
+class detrazioni_per_mantenimento_cane_guida(Variable):
+    value_type = float
+    entity = Persona
+    definition_period = YEAR
+    set_input = set_input_divide_by_period
+    label = "Detrazioni speciali per mantenimento cane guida"
+    reference = "http://www.agenziaentrate.gov.it/wps/wcm/connect/fcae4d804bb1ef709472f5d94f8d55f4/Annuario_online_Parte_III.pdf?MOD=AJPERES"  # Always use the most official source
+
+    def formula(person,period,parameters):
+        return where (person('spese_mantenimento_cane_guida',period),np.array(516.46),np.array(0))
+
+# TODO: calcolare correttamente le altre detrazioni
+class altre_detrazioni(Variable):
+    value_type = float
+    entity = Persona
+    definition_period = YEAR
+    set_input = set_input_divide_by_period
+    label = "Detrazioni speciali per borse di studio e donazioni ad enti ospedalieri TODO"
+    reference = "http://www.agenziaentrate.gov.it/wps/wcm/connect/fcae4d804bb1ef709472f5d94f8d55f4/Annuario_online_Parte_III.pdf?MOD=AJPERES"  # Always use the most official source
+
+# TODO: calcolare correttametne le detrazioni per investimenti in startup e decadenza startup
+
+class detrazioni_per_investimenti_startup(Variable):
+    value_type = float
+    entity = Persona
+    definition_period = YEAR
+    set_input = set_input_divide_by_period
+    label = "Detrazioni speciali per investimenti in startup TODO"
+    reference = "http://www.agenziaentrate.gov.it/wps/wcm/connect/fcae4d804bb1ef709472f5d94f8d55f4/Annuario_online_Parte_III.pdf?MOD=AJPERES"  # Always use the most official source
+
+
+class detrazioni_di_recupero_per_decadenza_startup(Variable):
+    value_type = float
+    entity = Persona
+    definition_period = YEAR
+    set_input = set_input_divide_by_period
+    label = "Detrazioni speciali per decadenza startup TODO"
+    reference = "http://www.agenziaentrate.gov.it/wps/wcm/connect/fcae4d804bb1ef709472f5d94f8d55f4/Annuario_online_Parte_III.pdf?MOD=AJPERES"  # Always use the most official source
