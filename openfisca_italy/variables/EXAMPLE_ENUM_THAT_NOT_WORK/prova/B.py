b689b9a39ef6ad77e333a78f0f90065e53fb55fe
Unknown <lorenzo.stacchio.dev@gmail.com>
2018-05-22 16:04:54 +0200

    Fix some errors

--- openfisca_italy/variables/EXAMPLE_ENUM_THAT_NOT_WORK/prova/B.py
+++ openfisca_italy/variables/EXAMPLE_ENUM_THAT_NOT_WORK/prova/B.py
@@ -3,7 +3,7 @@
 from openfisca_core.model_api import *
 # Import the entities specifically defined for this tax and benefit system
 from openfisca_italy.entita import *
-from openfisca_italy.variables.common import *
+from openfisca_italy.variables.EXAMPLE_ENUM_THAT_NOT_WORK.common import *
 
 
 class Variable_A(Variable):
